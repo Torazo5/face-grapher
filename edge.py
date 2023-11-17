b8038453a941f80d5faf01dfb2339a1f74487307
Torazo5 <torazo.tokuda@gmail.com>
2023-11-17 22:09:44 +0800

    better sobel
    +gaussian + proper formula

--- edge.py
+++ edge.py
@@ -1,25 +1,28 @@
 import cv2
+import numpy as np
 import matplotlib.pyplot as plt
 
-imgpath = "torazo3.jpg"
+imgpath = "torazo6.jpg"
 img = cv2.imread(imgpath, cv2.IMREAD_GRAYSCALE)  # Convert to grayscale
 
 # Resize the image to a lower quality
-new_height = 300
+new_height = 1000
 aspect_ratio = img.shape[1] / img.shape[0]
 new_width = int(new_height * aspect_ratio)
 resized_img = cv2.resize(img, (new_width, new_height))
 
-edgesx = cv2.Sobel(resized_img, -1, dx=1, dy=0, ksize=1)
-edgesy = cv2.Sobel(resized_img, -1, dx=0, dy=1, ksize=1)
-edges = edgesx + edgesy
+# Apply Gaussian blur
+blurred_img = cv2.GaussianBlur(resized_img, (5, 5), 0)  # You can adjust the kernel size (e.g., (5, 5)) and standard deviation (e.g., 0)
 
-output = [edges]
-titles = ['Edges']
+edgesx = cv2.Sobel(blurred_img, -1, dx=1, dy=0, ksize=1)
+edgesy = cv2.Sobel(blurred_img, -1, dx=0, dy=1, ksize=1)
 
-plt.imshow(output[0], cmap='gray')
-plt.title(titles[0])
-plt.xticks([])
-plt.yticks([])
+# Calculate the magnitude of edges using Euclidean distance
+edges_magnitude = np.sqrt(edgesx**2 + edgesy**2)
 
+# Increase sensitivity by multiplying by a constant factor (e.g., 2)
+sensitive_edges = edges_magnitude * 2
+
+
+plt.imshow(sensitive_edges, cmap='gray')
 plt.show()
